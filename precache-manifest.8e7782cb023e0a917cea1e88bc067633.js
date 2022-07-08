29e509cb6dea67bb8f766a96aae628c5b1f958b2
lidengke <lidengke0221@126.com>
2022-07-08 10:58:45 +0800

    deploy

---
+++ precache-manifest.8e7782cb023e0a917cea1e88bc067633.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720d030a4fd40fedb80d6fb816ff1d9f",
+    "url": "https://lidengkedev.github.io/react-web/index.html"
+  },
+  {
+    "revision": "8130f6832a65f5efcd7d",
+    "url": "https://lidengkedev.github.io/react-web/static/css/2.da393fb9.chunk.css"
+  },
+  {
+    "revision": "672fc22937c71619e16c",
+    "url": "https://lidengkedev.github.io/react-web/static/css/main.a0edd8f1.chunk.css"
+  },
+  {
+    "revision": "8130f6832a65f5efcd7d",
+    "url": "https://lidengkedev.github.io/react-web/static/js/2.d27c0192.chunk.js"
+  },
+  {
+    "revision": "f89a144118d10a7b0c54bc09cb08502f",
+    "url": "https://lidengkedev.github.io/react-web/static/js/2.d27c0192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "672fc22937c71619e16c",
+    "url": "https://lidengkedev.github.io/react-web/static/js/main.dd0de056.chunk.js"
+  },
+  {
+    "revision": "98403e3a697e132d1ac5",
+    "url": "https://lidengkedev.github.io/react-web/static/js/runtime-main.1f75d0f1.js"
+  }
+]);
